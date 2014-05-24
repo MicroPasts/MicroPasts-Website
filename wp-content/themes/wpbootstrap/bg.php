753ff21799f4ddecbd7af967bc1ae95c51f6fb32
Adi <adi.keinan.09@ucl.ac.uk>
2014-05-24 16:17:05 +0100

    Updating background image rotation code

--- wp-content/themes/wpbootstrap/bg.php
+++ wp-content/themes/wpbootstrap/bg.php
@@ -1,6 +1,5 @@
 <?php
-  $bg = array('http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/coins.jpg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/terracotta1.jpeg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/hoard.jpg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/pottery.jpg');
+  $bg = array('http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/coins.jpg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/terracotta5.jpeg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/hoard.jpg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/easter.jpeg','http://adi.micropasts.org/wp-content/uploads/sites/3/2014/05/testaccio.jpg');
   $i = rand(0, count($bg)-1);
   $selectedBg = $bg[$i];
 ?>
-
