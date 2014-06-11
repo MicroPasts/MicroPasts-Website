619626ac209a7a3882928690f6d44392732a5a7f
Adi <adi.keinan.09@ucl.ac.uk>
2014-06-11 15:48:45 +0100

    Adding grey border and Flickr link to footer

--- micropasts/footer.php
+++ micropasts/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-<hr>
+
     <div class="row">
        <div class="col-md-4"><a href="http://ucl.ac.uk/"><img src="http://micropasts.org/wp-content/uploads/2014/05/UCL-Logo.png" width="117" class="logoFooter"></a></div>
        <div class="col-md-4"><a href="http://www.britishmuseum.org/"><img src="http://micropasts.org/wp-content/uploads/2014/05/BM-Logo.png" width="100" class="logoFooter"></a></div>
@@ -13,6 +13,7 @@
              <li><i class="fa fa-envelope-o" style="color:white"></i> <a href="mailto:info@micropasts.org">Email</a>&nbsp;&nbsp;</li>
              <li><i class="fa fa-twitter" style="color:white"></i> <a href="http://twitter.com/micropasts">Twitter</a>&nbsp;&nbsp;</li>
              <li><i class="fa fa-facebook-square" style="color:white"></i> <a href="http://facebook.com/micropasts">Facebook</a></li>  
+             <li><i class="fa fa-flickr" style="color:white"></i> <a href="https://www.flickr.com/photos/micropasts">Flickr</a></li>
           </ul>
        </div>	 
     </div>  
