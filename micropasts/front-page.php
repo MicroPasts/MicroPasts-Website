cb46e059890daf6731b5a360b4a54257b8b80cc7
Daniel Pett <dpett@britishmuseum.org>
2017-10-28 11:39:38 +0100

    Amend front page

--- micropasts/front-page.php
+++ micropasts/front-page.php
@@ -5,7 +5,7 @@
    <div class="top-container"></div>
 
    <div class="row jumbotron">
-      <div class="col-md-12 mainpage">
+      <!-- <div class="col-md-12 mainpage">
          <p><h1>MicroPasts</h1></p>
          <p><h3>A community platform for conducting, designing and funding research into our human past.</h3></p>   
          <img src="http://micropasts.org/wp-content/uploads/2014/05/figurines_800px.png" class="figurines">       
@@ -27,7 +27,34 @@
          <h3>Discuss and design</h3>
          <p>Discuss existing research projects or design new ones on our community forum.</p>
          <p><a class="btn btn-primary" href="http://community.micropasts.org/">View details <i class="fa fa-chevron-right"></i></a></p>
-      </div>
+      </div> -->
+      
+      <?php
+$postlist = get_posts( 'orderby=menu_order&sort_order=asc' );
+$posts = array();
+foreach ( $postlist as $post ) {
+   $posts[] += $post->ID;
+}
+
+$current = array_search( get_the_ID(), $posts );
+$prevID = $posts[$current-1];
+$nextID = $posts[$current+1];
+?>
+
+<div class="navigation">
+<?php if ( !empty( $prevID ) ): ?>
+<div class="alignleft">
+<a href="<?php echo get_permalink( $prevID ); ?>"
+  title="<?php echo get_the_title( $prevID ); ?>">Previous</a>
+</div>
+<?php endif;
+if ( !empty( $nextID ) ): ?>
+<div class="alignright">
+<a href="<?php echo get_permalink( $nextID ); ?>" 
+ title="<?php echo get_the_title( $nextID ); ?>">Next</a>
+</div>
+<?php endif; ?>
+</div><!-- .navigation -->
    </div>
 
 </div>
